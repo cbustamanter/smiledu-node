1219e0898bdaf287e1424ffd50fe1a746d644a2f
cbustamanter <cesar.raggio1@gmail.com>
2021-03-29 23:07:27 -0500

    tasks completed

--- database/queries.js
+++ database/queries.js
@@ -1,35 +1,85 @@
-const Pool = require('pg').Pool
-const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
+const Pool = require("pg").Pool;
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASS,
   port: process.env.DB_PORT,
-})
-
+});
 
 const getGrades = (request, response) => {
-    pool.query('SELECT * FROM get_grados()', (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).json(results.rows)
-    })
-}
+  pool.query("SELECT * FROM get_grados()", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
 
 const getStudents = (request, response) => {
-    pool.exec
-    pool.query('SELECT * FROM grado ORDER BY orden ASC', (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).json(results.rows)
-    })
+  pool.query("SELECT * FROM get_persona()", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const getPayments = (request, response) => {
+  pool.query("SELECT * FROM get_pagos()", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const deleteStudent = (request, response) => {
+  const query = "call delete_persona($1)";
+  const value = [request.params.id];
+  pool.query(query, value, (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+
+const createStudent = async (request, response) => {
+  const bodyObject= JSON.parse(request.body.student)  
+  const query = "call create_persona($1, $2, $3, $4, $5, $6)";
+  const nom_persona = bodyObject.nom_persona;
+  const ape_pate_pers = bodyObject.ape_pate_pers;
+  const ape_mate_pers = bodyObject.ape_mate_pers;
+  const nid_grado = bodyObject.nid_grado;
+  const fecha_naci = bodyObject.fecha_naci;
+  let foto_ruta;
+  if (request.file) {
+    foto_ruta = `${request.file.destination}/${request.file.filename}`;
+  }  
+  const values = [
+    nom_persona,
+    ape_pate_pers,
+    ape_mate_pers,
+    nid_grado,
+    fecha_naci,
+    foto_ruta,
+  ];
+  pool.query(query, values, (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(foto_ruta);
+  });
 }
-  
 
 module.exports = {
-    getGrades,
-}
+  getGrades,
+  getStudents,
+  deleteStudent,
+  createStudent,
+  getPayments,
+};
--- index.js
+++ index.js
@@ -1,24 +1,46 @@
 const express = require('express')
 const db = require('./database/queries')
 const path = require('path')
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const sharp = require("sharp");
+const { v4: uuidv4 } = require('uuid');
+
 require('dotenv').config({ path: path.resolve(__dirname, '.env') })
 
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.json());
+app.use(express.json({
+  limit: '50mb', 
+  extended: true
+}));
 app.use(express.urlencoded({
+  limit: '50mb',
   extended: true
 }));
 
-
-app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
+const storage = multer.diskStorage({
+  destination: (req, file, callBack) => {
+      callBack(null, 'upload')
+  },
+  filename: (req, file, callBack) => {
+      callBack(null, `${uuidv4()}_${file.originalname}`)
+  }
 })
 
-app.get('/grados', db.getGrades)
+const upload = multer({ storage: storage })
+
+app.use('/upload',express.static('./upload/'))
+
+app.get('/api/grados', db.getGrades)
+app.get('/api/students', db.getStudents)
+app.get('/api/payments', db.getPayments)
+
+app.delete('/api/student/delete/:id', db.deleteStudent)
+
+app.post('/api/student/new', upload.single('file'), db.createStudent)
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
-    console.log(process.env.DB_USER)
 })
